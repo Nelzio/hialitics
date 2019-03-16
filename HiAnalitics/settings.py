2eb51610606368e23867d4c3996c11e9f6584333
unknown <nelziositoe@gmail.com>
2019-03-16 20:09:20 +0200

    asfad

--- HiAnalitics/settings.py
+++ HiAnalitics/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'djoser',
     'rest_framework.authtoken',
     'accounts.apps.AccountsConfig',
     'rest_framework',
